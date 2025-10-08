a4ac841d2db8d4710f83aa01c186c3bb6279c7d9
Abdusattorov Dadajon <abdusattorovdadajon07@gmail.com>
2025-10-08 10:07:30 +0500

    first assignment

---
+++ player_card.py
@@ -0,0 +1,25 @@
+name = input("Enter player name: ")
+team = input("Enter your team: ")
+position = input("Enter your position: ")
+number = input("Enter your shirt number: ")
+hometown = input("Enter your hometown: ")
+
+
+
+
+
+print("########################################")
+print("LEGENDS OF THE LEAGUE")
+print("########################################")
+
+print(f"Player: {name}")
+print(f"Team : {team}")
+print(f"Position: {position}")
+print(f"Number: {number}")
+
+print("----------------------------------------")
+print(f"Player Tag: {number}-{team}-{name}")
+print(f"Hometown: {hometown}")
+
+print("----------------------------------------")
+print("########################################")

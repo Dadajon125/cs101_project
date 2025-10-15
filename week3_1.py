7bfed74845f66b3f183920aa1e06f8500269bfa3
Abdusattorov Dadajon <abdusattorovdadajon07@gmail.com>
2025-10-15 09:13:40 +0500

    week3

---
+++ week3_1.py
@@ -0,0 +1,13 @@
+
+print("---Movie Ticket Pricer---")
+a = int(input("please enter your age: " ))
+if a <= 12:
+
+    print("your ticket price is: $8")
+elif 13<=a<=64:
+
+    print ("you fall into the 'Adult' category") 
+    print("your ticket price is: $10")
+else:
+    print("you fall into the 'Adult' category")
+    print("your ticket price is: $15")
